ed916d52e1f9e04bf9ab2e3bd8d5c621e93cb322
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2023-01-07 19:19:23 -0800

    modification to make pycodestyle happy

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
     import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    arr = dir(hidden_4)
+    for i in range(len(arr)):
+        if (arr[i][:2] != "__"):
+            print(arr[i])
