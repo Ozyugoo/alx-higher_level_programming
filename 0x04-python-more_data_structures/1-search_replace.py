98ba07761ef93ea8196e40d5952c9782d2ad3602
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2023-01-31 06:11:03 -0800

    more pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ A function that replaces all occurrences of an element """
+
+
 def search_replace(my_list, search, replace):
-
-
     new_list = []
     for i in my_list:
         if i == search:
