60d38c76388da318ec9a17e080556d25af769073
Ugoo-Okonkwo Stella <stellaozy22@gmail.com>
2023-02-06 07:28:39 -0800

    Indentation and pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,12 +2,12 @@
 """ A class that inherits from BaseGeometry """
 
 
- BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
- class Rectangle(BaseGeometry):
-     """ Rectangle class that inherits from BaseGeometry """
-     def __init__(self, width, height):
+class Rectangle(BaseGeometry):
+    """ Rectangle class that inherits from BaseGeometry """
+    def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
